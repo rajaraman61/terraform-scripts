1d4d51185aa9a20b270770eed5e7920d13fd6af9
rajaraman61 <rajaraman61@outlook.com>
2022-08-27 20:27:32 +0530

    Bucket With VPC added

--- aws/s3/main.tf
+++ aws/s3/main.tf
@@ -1,3 +1,4 @@
+# Bucket without VPC
 resource "aws_s3_bucket" "awsbuck1" {
   bucket = "my-tf-aws-bucket"
 
@@ -5,4 +6,31 @@ resource "aws_s3_bucket" "awsbuck1" {
     Name        = "storage bucket"
     Environment = var.environment
   }
+}
+
+# Bucket With VPC
+resource "aws_s3_bucket" "fb_media_bucket" {
+    bucket = var.bucketMedia
+}
+
+resource "aws_s3_bucket_policy" "allow_access_to_specific_vpce_only" {
+  bucket = aws_s3_bucket.fb_media_bucket.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Sid": "Access-to-specific-VPCE-only",
+        "Principal": "*",
+        "Action": "s3:*",
+        "Effect": "Deny",
+        "Resource": ["${aws_s3_bucket.fb_media_bucket.arn}",
+                    "${aws_s3_bucket.fb_media_bucket.arn}/*"],
+        "Condition": {
+            "StringNotEquals": {
+            "aws:SourceVpce": "${aws_vpc.vpc_end_point.id}"
+            }
+        }
+    }
+    ]
+  })
 }
--- aws/s3/variables.tf
+++ aws/s3/variables.tf
@@ -2,3 +2,7 @@ variable "Environment" {
     description = "This is the cloud hosting region where your app will be deployed."
     default = "develop"
 }
+
+variable "bucketMedia" {
+  default = "fb-vpc-media-bucket" # USE A UNIQUE BUCKET NAME
+}
